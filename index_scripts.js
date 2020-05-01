0428e92965fd59287a04b62f8925267e98508d0e
zilzilok <tnsafin@edu.hse.ru>
2020-05-02 02:53:18 +0500

    added new html page

--- index_scripts.js
+++ index_scripts.js
@@ -3,7 +3,9 @@ function populate() {
         let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
         if (windowRelativeBottom > document.documentElement.clientHeight + 100) break;
         document.body.insertAdjacentHTML("beforeend",
-            `<div class="block" style="background-color:#${Math.floor(Math.random() * 16777215).toString(16)}">ТЫ ПИДОР!</div>`);
+            `<div class="block"
+             style="background-color:#${Math.floor(Math.random() * 16777215).toString(16)}"
+             onclick="new_page()">ТЫ ПИДОР</div>`);
     }
 }
 
@@ -14,4 +16,9 @@ setInterval(
     }, 1000);
 
 window.addEventListener('scroll', populate);
+
+function new_page() {
+    location.href = "flex.html"
+}
+
 populate(); // инициализация документа
