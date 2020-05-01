38032bc96f5383256ee18cc8011369ae504c2f1c
zilzilok <tnsafin@edu.hse.ru>
2020-05-02 00:51:10 +0500

    chet tam

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ function populate() {
 setInterval(
     function() {
         var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        document.body.style.backgroundColor = "#" + randomColor;
+        document.body.style.backgroundColor = `#${randomColor}`;
     }, 1000);
 
 window.addEventListener('scroll', populate);
